1ed367cbb79133e05445468d6d9fbda2498b70e0
Andrey Kitaev <a.kitaev@hot-wifi.ru>
2022-01-02 22:14:59 +0300

    WIP: lib implementation

--- smart_home/src/room.rs
+++ smart_home/src/room.rs
@@ -4,7 +4,9 @@ use std::error::Error;
 use std::fmt::{Display, Formatter};
 
 #[derive(Debug)]
-pub struct RoomReadResult {}
+pub struct RoomReadResult {
+    err: Option<Box<dyn Error>>,
+}
 
 impl Display for RoomReadResult {
     fn fmt(&self, _f: &mut Formatter<'_>) -> std::fmt::Result {
@@ -15,7 +17,9 @@ impl Display for RoomReadResult {
 impl Error for RoomReadResult {}
 
 #[derive(Debug)]
-pub struct RoomUpdateResult {}
+pub struct RoomUpdateResult {
+    err: Option<Box<dyn Error>>,
+}
 
 impl Display for RoomUpdateResult {
     fn fmt(&self, _f: &mut Formatter<'_>) -> std::fmt::Result {
@@ -31,20 +35,64 @@ pub struct Room {
 }
 
 impl Room {
-    pub fn add_device(&mut self, _name: String, _device: Box<dyn Device>) -> RoomUpdateResult {
-        todo!()
+    pub fn new(name: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            devices: HashMap::new(),
+        }
     }
-    pub fn remove_device(&mut self, _name: &str) -> RoomUpdateResult {
-        todo!()
+    pub fn add_device(&mut self, name: &str, device: Box<dyn Device>) -> RoomUpdateResult {
+        let mut err = None;
+
+        if self.devices.contains_key(name) {
+            err = Some(string_error::into_err(format!("room already contains device '{}'", name)));
+        } else {
+            self.devices.insert(name.to_string(), device);
+        }
+        RoomUpdateResult { err }
     }
-    pub fn list_devices(&self) -> Vec<Box<dyn Device>> {
-        todo!()
+
+    pub fn remove_device(&mut self, name: &str) -> RoomUpdateResult {
+        let mut err = None;
+
+        if !self.devices.contains_key(name) {
+            err = Some(string_error::into_err(format!("room does not contain device '{}'", name)))
+        } else {
+            self.devices.remove(name);
+        }
+        RoomUpdateResult { err }
     }
+
+    pub fn list_devices(&self) -> Vec<&dyn Device> {
+        let mut out = vec![];
+
+        for device in self.devices.values() {
+            out.push(device.as_ref())
+        }
+
+        out
+    }
+
     pub fn get_device(&self, _name: &str) -> Result<Box<dyn Device>, RoomReadResult> {
         todo!()
     }
+
     pub fn get_summary(&self) -> String {
-        todo!()
+        let mut out = String::new();
+
+        for device in self.devices.values() {
+            let status = device.get_status();
+            match status {
+                Ok(s) => {
+                    out.push_str(s.as_string())
+                },
+                Err(err) => {
+                    out.push_str(format!("error getting status: {}", err).as_str())
+                },
+            }
+        }
+
+        out
     }
 }
 
