24a54c562cddff955d721f5790b0441a5e633e7e
Andrey Kitaev <a.kitaev@hot-wifi.ru>
2022-01-07 14:27:03 +0300

    cargo fmt

--- thermometer_server/src/main.rs
+++ thermometer_server/src/main.rs
@@ -1,6 +1,6 @@
-use std::net::UdpSocket;
 use rand::Rng;
 use s_home_proto::{DeviceAction, DeviceRequest, Marshal, Response};
+use std::net::UdpSocket;
 use std::thread::{sleep, spawn};
 use std::time::Duration;
 
@@ -10,7 +10,7 @@ struct State {
 }
 
 fn main() {
-    let mut state = State{
+    let mut state = State {
         is_on: true,
         temp: 20.0,
     };
@@ -25,7 +25,7 @@ fn main() {
     });
 
     let socket = UdpSocket::bind("127.0.0.1:1235").unwrap();
-    let mut buf = [0;1024];
+    let mut buf = [0; 1024];
 
     loop {
         match socket.recv_from(&mut buf) {
@@ -33,28 +33,26 @@ fn main() {
                 let s = String::from_utf8_lossy(&buf).to_string();
                 let req = DeviceRequest::unmarshal(&s).unwrap();
                 let resp = match req {
-                   DeviceRequest::Ping => Response::Pong,
-                   DeviceRequest::Status => {
-                       let (status, temp) = match state.is_on {
-                           true => ("on", state.temp),
-                           false => ("off", 0.0),
-                       };
-                       Response::Status(format!("status: {}, temp: {:.1}", status, temp))
-                   } 
-                   DeviceRequest::DeviceAction {method}=> {
-                       match method {
+                    DeviceRequest::Ping => Response::Pong,
+                    DeviceRequest::Status => {
+                        let (status, temp) = match state.is_on {
+                            true => ("on", state.temp),
+                            false => ("off", 0.0),
+                        };
+                        Response::Status(format!("status: {}, temp: {:.1}", status, temp))
+                    }
+                    DeviceRequest::DeviceAction { method } => {
+                        match method {
                             DeviceAction::TurnOff => state.is_on = false,
-                            DeviceAction::TurnOn => state.is_on = true    
-                       };
-                       Response::Ok
-                   }
+                            DeviceAction::TurnOn => state.is_on = true,
+                        };
+                        Response::Ok
+                    }
                 };
                 let message = resp.marshal();
                 socket.send(message.as_bytes().as_ref()).unwrap();
-            },
-            Err(e) => println!("err receiving a datagram: {}", &e)
-            
+            }
+            Err(e) => println!("err receiving a datagram: {}", &e),
         }
     }
-    
 }
